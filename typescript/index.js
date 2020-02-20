bd39d8b4482bd8897e99d100d97cc44c8203689c
Chayut Ruksomya <chayut.r@bluepi.co.th>
2020-02-20 15:55:36 +0700

    Basic type in typescript

--- typescript/index.js
+++ typescript/index.js
@@ -1,1 +1,13 @@
-console.log('Hello TypeScript');
+function add(a, b, showResult, resultPrefix) {
+    if (resultPrefix === void 0) { resultPrefix = ''; }
+    if (showResult) {
+        console.log(resultPrefix + a + b);
+    }
+    else {
+        return a + b;
+    }
+}
+var num1 = 1;
+var num2 = 2.00;
+var prefix = 'Result : ';
+var result = add(num1, num2, true, prefix);
--- typescript/index.ts
+++ typescript/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello TypeScript')
+function add(a: number, b: number, showResult: boolean, resultPrefix: string = '') {
+    if (showResult) {
+        console.log(resultPrefix + (a + b))
+    } else {
+        return a + b
+    }
+}
+
+const num1 = 1
+const num2 = 2.00
+const prefix = 'Result : '
+
+const result = add(num1, num2, true, prefix)
+
