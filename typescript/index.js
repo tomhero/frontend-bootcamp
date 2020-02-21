92aed89160f906ceb4296a8b12bbcf2a4182a182
Chayut Ruksomya <chayut.191@gmail.com>
2020-02-21 16:45:37 +0700

    [ts] Literal Types

--- typescript/index.js
+++ typescript/index.js
@@ -1,19 +1,19 @@
 /**
- * Union Types
- * Literal Types
+ * Union Types --> arg1 : string | number
+ * Literal Types --> arg2 : 'my custom val1' | 'mu csutom val2'
  * Type Aliases
  */
-function combine(input1, input2) {
+function combine(input1, input2, returnType) {
     var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2;
+    if (typeof input1 === 'number' && typeof input2 === 'number' || returnType === 'int') {
+        result = (+input1) + (+input2);
     }
     else {
         result = input1.toString() + input2.toString();
     }
     return result;
 }
-var combinedNum = combine(25, 50);
+var combinedNum = combine(25, 50, 'int');
 console.log(combinedNum);
-var combinedName = combine('Tom', 'NgaiJa');
+var combinedName = combine('Tom', 'NgaiJa', 'str');
 console.log(combinedName);
--- typescript/index.ts
+++ typescript/index.ts
@@ -1,21 +1,22 @@
 /**
- * Union Types
- * Literal Types
+ * Union Types --> arg1 : string | number
+ * Literal Types --> arg2 : 'my custom val1' | 'mu csutom val2'
  * Type Aliases
  */
 
-function combine(input1: string | number, input2: string | number) {
+function combine(
+    input1: string | number, input2: string | number, returnType: 'str' | 'int') {
     let result: string | number;
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2;
+    if (typeof input1 === 'number' && typeof input2 === 'number' || returnType === 'int') {
+        result = (+input1) + (+input2);
     } else {
         result = input1.toString() + input2.toString()
     }
     return result;
 }
 
-const combinedNum = combine(25, 50)
+const combinedNum = combine(25, 50, 'int')
 console.log(combinedNum);
 
-const combinedName = combine('Tom', 'NgaiJa');
+const combinedName = combine('Tom', 'NgaiJa', 'str');
 console.log(combinedName);
