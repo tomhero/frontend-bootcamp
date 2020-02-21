0aa474b8497288989833dcf0813657fd3d7102df
Chayut Ruksomya <chayut.191@gmail.com>
2020-02-21 16:06:30 +0700

    [typescript] simulate loop and type check / tuple

--- typescript/index.js
+++ typescript/index.js
@@ -3,15 +3,22 @@
  * Array
  * any
  * Tuple
+ * Enum (Java like)
  */
 var person = {
     name: 'Chayut',
     nickname: 'Tom',
     age: 23,
-    foods: undefined
+    foods: ['Steak', 'Omlet', 'Som Tum', 'Tom-Yum-Kung'],
+    role: [2, 'Dev']
 };
+// person.role[1] = 123 // ❌ you cannot do this because of tuple of [number, string]
 var favFoods; // array declaration
-//  favFoods = ['egg!!', 2] // ❌ you cannot do this
+//  favFoods = ['egg!!', 2] // ❌ you cannot do this because this array accepts string only
 var anyFavFoods;
 anyFavFoods = ['egg', 2, false];
-console.log(person.nickname);
+for (var _i = 0, _a = person.foods; _i < _a.length; _i++) {
+    var food = _a[_i];
+    // TypeScript knows what is type for food variable!!
+    console.log(food.toUpperCase());
+}
--- typescript/index.ts
+++ typescript/index.ts
@@ -3,24 +3,31 @@
  * Array
  * any
  * Tuple
+ * Enum (Java like)
  */
 
- const person: {
-     name: string
-     age: number
-     nickname: string
-     foods: ['Steak', 'Omlet'] // array
- } = {
-     name: 'Chayut',
-     nickname: 'Tom',
-     age: 23,
-     foods: undefined
- }
+const person: {
+    name: string
+    age: number
+    nickname: string
+    foods: string[] // array of string
+    role: [number, string] // tuple 
+} = {
+    name: 'Chayut',
+    nickname: 'Tom',
+    age: 23,
+    foods: ['Steak', 'Omlet', 'Som Tum', 'Tom-Yum-Kung'],
+    role: [2, 'Dev']
+}
+// person.role[1] = 123 // ❌ you cannot do this because of tuple of [number, string]
 
- let favFoods: string[] // array declaration
-//  favFoods = ['egg!!', 2] // ❌ you cannot do this
+let favFoods: string[] // array declaration
+//  favFoods = ['egg!!', 2] // ❌ you cannot do this because this array accepts string only
 let anyFavFoods: any[]
 anyFavFoods = ['egg', 2, false]
 
- console.log(person.nickname);
- 
+for (const food of person.foods) {
+    // TypeScript knows what is type for food variable!!
+    console.log(food.toUpperCase());
+}
+
