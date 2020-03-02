707c9033500fedbe141e540eadfc1f9db293ea91
Chayut Ruksomya <chayut.r@bluepi.co.th>
2020-03-02 11:41:06 +0700

    [ts] null type strict

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,1 +1,7 @@
 console.log('Hello TypeScript');
+
+const button = document.querySelector('button')!; // <- This "!" sign is very useful
+
+button.addEventListener('click', (e) => {
+    console.log(e);
+});
