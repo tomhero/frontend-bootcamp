b9fa21acff38179b59e41d06ccd5793e0a22e698
Chayut Ruksomya <chayut.191@gmail.com>
2020-03-03 12:02:24 +0700

    Add private scope

--- typescript/src/classes-interfaces/index.ts
+++ typescript/src/classes-interfaces/index.ts
@@ -1,25 +1,33 @@
 class Department {
 
-    name: string;
-    employees: string[] = [];
+    private name: string;
+    private employees: string[] = [];
 
     constructor(name: string) {
         this.name = name
     }
 
-    describe() {
-        console.log(`Departmant name is ${this.name}`);
+    describe(needSecret: boolean) {
+        if (needSecret) {
+            console.log(`Departmant name is ${this.name} | ${this.getDepartmentSecret()}`);
+        } else {
+            console.log(`Departmant name is ${this.name}`);
+        }
     }
 
     addEmployee(employee: string) {
         this.employees.push(employee)
     }
+
+    private getDepartmentSecret() {
+        return 'TopSecret!!'
+    }
     
 }
 
 const myDepartment = new Department("SD");
-myDepartment.addEmployee("Tom")
+myDepartment.addEmployee("Tom");
+myDepartment.addEmployee("Maya")
 
-myDepartment.describe()
-
-
+myDepartment.describe(true)
+myDepartment.describe(false)
