2d24cfe3f4e7b5accc10b6a528060b0f7a482947
Chayut Ruksomya <chayut.191@gmail.com>
2020-03-03 12:30:39 +0700

    [ts] Inheritance

--- typescript/src/classes-interfaces/index.ts
+++ typescript/src/classes-interfaces/index.ts
@@ -29,9 +29,45 @@ class Department {
     
 }
 
-const myDepartment = new Department('123', 'SD');
+class DigitalDepartment extends Department {
+    admins: string[]
+    constructor(id: string, admins: string[]) {
+        super(id, 'Digital') // this line of code should reside here!!
+        this.admins = admins
+    }
+}
+
+class AccountingDepartment extends Department {
+    constructor(id: string, private reports: string[]) {
+        super(id, 'Accounting')
+    }
+
+    addReport(report: string) {
+        this.reports.push(report)
+    }
+
+    printReport() {
+        console.log(this.reports);
+        
+    }
+}
+
+const myDepartment = new Department('Dev1', 'SD');
 myDepartment.addEmployee("Tom");
 myDepartment.addEmployee("Maya")
 
 myDepartment.describe(true)
 myDepartment.describe(false)
+
+const preSellDept = new DigitalDepartment('Dig1', [
+    'B1',
+    'B2'
+]);
+
+console.log(preSellDept.admins);
+
+const accounting = new AccountingDepartment('ac1', [])
+
+accounting.addReport('mayday mayday!!')
+
+accounting.printReport()
