ef1eb56a64d0c2cfc8159ac0c4bbf3e5ccc49bb8
Chayut Ruksomya <chayut.191@gmail.com>
2020-03-03 13:58:39 +0700

    [ts] finished class/interface section

--- typescript/src/classes-interfaces/index.ts
+++ typescript/src/classes-interfaces/index.ts
@@ -4,13 +4,13 @@ interface AddFn {
     (a: number, b: number): number;
 }
 
-let add: AddFn;
+let myAdd: AddFn;
 
-add = (n1: number, n2: number) => {
+myAdd = (n1: number, n2: number) => {
     return n1 + n2
 }
 
-console.log(add(1, 2));
+console.log(myAdd(1, 2));
 
 
 // interface is a custom type!! you could say.
