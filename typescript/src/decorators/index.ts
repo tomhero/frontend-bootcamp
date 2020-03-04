55b26bd9a1e4eddcd666fee0cf42ec849468c008
Chayut Ruksomya <chayut.191@gmail.com>
2020-03-04 14:39:32 +0700

    [ts] decorator hooks

--- typescript/src/decorators/index.ts
+++ typescript/src/decorators/index.ts
@@ -1,13 +1,16 @@
 function Logger(logString: string) { // This function is a decorator factory
+    console.log('Logger has been called'); // A.
     return (constructor: Function) => {
-        console.log('logging...' + logString);
-        console.log(constructor);
+        console.log('logging...' + logString); // 2.
+        console.log(constructor); // 3.
     }
 }
 
 function withTemplate(template: string, hookID: string) {
+    console.log('withTemplate has been called'); // B.
     // return (_: Function) => { // `_` variable means not use but decorator need at least one args | LOL
     return (constructor: any) => {
+        console.log('rendering...'); // 1.
         const hookEl = document.getElementById(hookID);
         const person = new constructor();
         if (hookEl) {
@@ -18,8 +21,9 @@ function withTemplate(template: string, hookID: string) {
 }
 
 { // This is only block scope from JS knowledge \ no need to rousing
-    @Logger('log text test') // Run when a particular class has been defined!!
-    @withTemplate('<h1>Human Header!! here</h1>', 'app')
+    // Decorator runs when a particular class has been defined!!
+    @Logger('log text test') //  A. 2. 3.
+    @withTemplate('<h1>Human Header!! here</h1>', 'app') // B. 1.
     class Human {
         name = 'Tomhero'
     
