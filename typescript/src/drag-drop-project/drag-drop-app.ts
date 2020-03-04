1d4d1aed871a72b4282daa8fd435a58ffb0a21b7
Chayut Ruksomya <chayut.191@gmail.com>
2020-03-04 20:57:06 +0700

    [ts] Add render list and init state class

--- typescript/src/drag-drop-project/drag-drop-app.ts
+++ typescript/src/drag-drop-project/drag-drop-app.ts
@@ -14,16 +14,16 @@ function validateInput(validatableInput: Validatable): boolean {
         isValid = isValid && validatableInput.value.toString().length !== 0;
     }
     if ( // Because if `!= null` is also include check for undefine!!
-        validatableInput.minLength != null && 
+        validatableInput.minLength != null &&
         typeof validatableInput.value === 'string'
-        ) {
-            isValid = isValid && validatableInput.value.length >= validatableInput.minLength;
+    ) {
+        isValid = isValid && validatableInput.value.length >= validatableInput.minLength;
     }
     if (
-        validatableInput.maxLength != null && 
+        validatableInput.maxLength != null &&
         typeof validatableInput.value === 'string'
-        ) {
-            isValid = isValid && validatableInput.value.length <= validatableInput.maxLength;
+    ) {
+        isValid = isValid && validatableInput.value.length <= validatableInput.maxLength;
     }
 
     if (validatableInput.min != null && typeof validatableInput.value === 'number') {
@@ -50,6 +50,61 @@ function Autobind(_target: any, _methodName: string, descriptor: PropertyDescrip
     return adjDescriptor;
 }
 
+class ProjectState {
+    private projects: any[] = [];
+
+    addProject(title: string, description: string, numOfpeople: number) {
+        const newProject = {
+            id: Math.random().toString(),
+            title,
+            description,
+            people: numOfpeople
+        };
+        this.projects.push(newProject);
+    }
+}
+
+class ProjectList {
+    templateEl: HTMLTemplateElement;
+    renderTargetEl: HTMLDivElement;
+    rootEl: HTMLElement;
+
+    constructor(
+        templateElId: string,
+        targetElId: string,
+        private type: 'active' | 'finished'
+    ) {
+        this.templateEl = document.getElementById(templateElId) as HTMLTemplateElement;
+        this.renderTargetEl = document.getElementById(targetElId) as HTMLDivElement;
+        this.rootEl = this.templateEl.content.querySelector('section')!.cloneNode(true) as HTMLFormElement;
+
+        this.configure();
+        this.render();
+        this.renderItems();
+    }
+
+    /**
+ * Configure an element before render
+ */
+    private configure(): void {
+        this.rootEl.id = `${this.type}-projects`
+    }
+
+    /**
+     * Render templateEl into targetEl on screen
+     */
+    private render(): void {
+        this.renderTargetEl.insertAdjacentElement('beforeend', this.rootEl);
+    }
+
+    private renderItems() {
+        const listId = `${this.type}-project-list`;
+        this.rootEl.querySelector('ul')!.id = listId;
+        this.rootEl.querySelector('h2')!.textContent = this.type.toUpperCase() + ' PROJECT';
+    }
+
+}
+
 class ProjectInput {
     // HTMLTemplateElement is a interface from "DOM" lib
     templateEl: HTMLTemplateElement;
@@ -62,8 +117,8 @@ class ProjectInput {
     constructor(templateElId: string, targetElId: string) {
         this.templateEl = document.getElementById(templateElId)! as HTMLTemplateElement;
         this.renderTargetEl = <HTMLDivElement>document.getElementById(targetElId)!;
-        // Get document fragment (child elements)
-        this.rootEl = this.templateEl.content.querySelector('form') as HTMLFormElement;
+        // Get document fragment (child elements) with deep copy!!
+        this.rootEl = this.templateEl.content.querySelector('form')!.cloneNode(true) as HTMLFormElement;
 
         this.titleInputEl = this.rootEl.querySelector('#title') as HTMLInputElement;
         this.descriptionInputEl = this.rootEl.querySelector('#description') as HTMLInputElement;
@@ -151,3 +206,5 @@ class ProjectInput {
 }
 
 const projectInput = new ProjectInput('project-input', 'app');
+const acativeProjects = new ProjectList('project-list', 'app', 'active');
+const finishedProject = new ProjectList('project-list', 'app', 'finished');
