fc0d3c4325821756e3a16e03de3be0fd56798975
Chayut Ruksomya <chayut.191@gmail.com>
2020-03-04 15:24:28 +0700

    [ts] Returning (and changing) a Class

--- typescript/src/decorators/index.ts
+++ typescript/src/decorators/index.ts
@@ -9,13 +9,19 @@ function Logger(logString: string) { // This function is a decorator factory (in
 function WithTemplate(template: string, hookID: string) {
     console.log('withTemplate has been called'); // B.
     // return (_: Function) => { // `_` variable means not use but decorator need at least one args | LOL
-    return (constructor: any) => {
-        console.log('rendering...'); // 1.
-        const hookEl = document.getElementById(hookID);
-        const person = new constructor();
-        if (hookEl) {
-            hookEl.innerHTML = template;
-            hookEl.querySelector('h1')!.textContent = person.name; // `hookEl.querySelector('h1')` will never null
+    return <T extends {new (...args: any[]): { name: string } }>(originalConstructor: T) => {
+        return class extends originalConstructor {
+            // This is a returnning of new custom class with some logic below!!
+            constructor(..._args: any[]) { // `_args` means not use args variable in later
+                super();
+                console.log('rendering...'); // 1.
+                const hookEl = document.getElementById(hookID);
+                // const person = new originalConstructor();
+                if (hookEl) {
+                    hookEl.innerHTML = template;
+                    hookEl.querySelector('h1')!.textContent = this.name; // `hookEl.querySelector('h1')` will never null
+                }
+            }
         }
     }
 }
@@ -25,7 +31,7 @@ function WithTemplate(template: string, hookID: string) {
     @Logger('log text test') //  A. 2. 3.
     @WithTemplate('<h1>Human Header!! here</h1>', 'app') // B. 1.
     class Human {
-        name = 'Superman'
+        name: string = 'Superman'
     
         constructor() {
             console.log('Create some human');
