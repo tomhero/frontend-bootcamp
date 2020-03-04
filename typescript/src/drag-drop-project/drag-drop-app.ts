1b34d9bfb30c3b483069ec0bbb175ede9d0095f1
Chayut Ruksomya <chayut.r@bluepi.co.th>
2020-03-04 18:45:47 +0700

    [ts] first tiem to render template element

--- typescript/src/drag-drop-project/drag-drop-app.ts
+++ typescript/src/drag-drop-project/drag-drop-app.ts
@@ -1,1 +1,24 @@
-console.log('Welcome to drag and drop app!!');
+// console.log('Welcome to drag and drop app!!');
+
+class ProjectInput {
+    // HTMLTemplateElement is a interface from "DOM" lib
+    templateEl: HTMLTemplateElement;
+    renderTargetEl: HTMLDivElement;
+
+    constructor(templateElId: string, targetElId: string) {
+        this.templateEl = document.getElementById(templateElId)! as HTMLTemplateElement;
+        this.renderTargetEl = <HTMLDivElement>document.getElementById(targetElId)!;
+        this.render()
+    }
+
+    /**
+     * render targetEl to screen
+     */
+    private render(): void {
+        // Get document fragment (child elements)
+        const elementsToRender = this.templateEl.content.querySelector('form')! as HTMLFormElement;
+        this.renderTargetEl.innerHTML = elementsToRender.outerHTML
+    }
+}
+
+const projectInput = new ProjectInput('project-input', 'app');
