c46d3a5f0ee2e43859b9ffc2e757ed581197894c
Chayut Ruksomya <chayut.191@gmail.com>
2020-03-04 11:28:37 +0700

    [ts] Advance generic type / function

--- typescript/src/generic/index.ts
+++ typescript/src/generic/index.ts
@@ -18,7 +18,8 @@ myProcess.then(data => {
 
 // Example of Generic function!!
 // Now merge function can accept all object structure as args and TypeScript will reconize it!!
-function merge<T, U>(objA: T, objB: U) {
+// Add also if you use `extends` keyword to add Constraints to an args
+function merge<T extends object, U extends object>(objA: T, objB: U) {
     return Object.assign(objA, objB)
 }
 
@@ -26,7 +27,26 @@ function merge<T, U>(objA: T, objB: U) {
 // const mergeResult = merge({name: 'Tom'}, {age: 23}) as {name: string ; age: number};
 const mergeResult = merge({name: 'Tom'}, {age: 23});
 const mergeResult2 = merge({name: 'Tom', favFood: 'steak'}, {beverage: 'soft drink'});
+// const mergeResult3 = merge({name: 'Tom'}, 999); // you cannot do this because of `extends` keyword in function
 console.log(mergeResult.name);
 console.log(mergeResult2.favFood);
 
+interface Lengthy {
+    length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): {el: T, description: string} {
+    let desc = 'Got no value.';
+    const elLength = element.length; // this is a valid code bacause of `extends Lengthy` keyword
+    if (elLength === 1) {
+        desc = 'Got one element.';
+    } else if (elLength > 1) {
+        desc = `Got ${elLength} elements.`;
+    }
+    return {el: element, description: desc}
+}
+
+console.log(countAndDescribe('qwerty'));
+// You can also call it with array type!! wow §(*￣▽￣*)§
+console.log(countAndDescribe(['qwerty', '123456', 999]));
 
