82445265a262dd29d90b749b6c5a5a8aa86245fd
Chayut Ruksomya <chayut.r@bluepi.co.th>
2020-03-10 15:52:47 +0700

    [react] Add global example for css module

--- react-js/first-react-app/src/Person/Person.js
+++ react-js/first-react-app/src/Person/Person.js
@@ -2,8 +2,9 @@ import React from 'react';
 import personStyle from './Person.module.css';
 
 const person = (props) => {
+    console.log(personStyle);
     return (
-            <div className={personStyle['person-item']}>
+            <div className="person-item">
                 <p onClick={props.click}>I'm Person | {props.name} | {props.age} | {props.children}</p>
                 <input onChange={props.change} value={props.name} type="text" />
             </div>
