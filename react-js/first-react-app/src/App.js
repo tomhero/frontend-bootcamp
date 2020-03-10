9ce6b7b5f320c8eef973c7741f97414ea7f838d0
Chayut Ruksomya <chayut.r@bluepi.co.th>
2020-03-10 12:50:20 +0700

    [react] Add conditional rendering

--- react-js/first-react-app/src/App.js
+++ react-js/first-react-app/src/App.js
@@ -5,15 +5,17 @@ import Person from './Person/Person'
 
 class App extends React.Component {
 
- state = {
-   persons: [
-     { name: 'Tom', age: 23 },
-     { name: 'Que', age: 29 },
-     { name: 'Art', age: 30 }
-   ],
-   otherState: 'some others state!!?'
+  state = {
+    persons: [
+      { name: 'Tom', age: 23 },
+      { name: 'Que', age: 29 },
+      { name: 'Art', age: 30 }
+    ],
+    showPerson: true,
+    otherState: 'some others state!!?'
   }
 
+  // In a class component ou need to use arrow function as a handler
   switchNameHandler = (newName) => {
     this.setState({
       persons: [
@@ -24,12 +26,18 @@ class App extends React.Component {
     })
   }
 
-  render () {
+  toggleHandler = () => {
+    console.log('Toggling...');
+    this.setState({
+      showPerson: !this.state.showPerson
+    });
+  }
+
+  render() {
     const style = {
       backgroundColor: 'white',
-      font: 'inherit',
       border: '1px solid blue',
-      padding: '8px',
+      padding: '4px',
       cursor: 'pointer'
     };
 
@@ -48,18 +56,29 @@ class App extends React.Component {
           <br></br>
           <button style={style} onClick={
             () => this.switchNameHandler(this.state.persons[0].name.toUpperCase())
-            }>
+          }>
             Click for UPPER
           </button>
           <br />
           <button style={style} onClick={
             this.switchNameHandler.bind(this, this.state.persons[0].name.toLowerCase())
-            }>
+          }>
             Click for lower
           </button>
-          <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-          <Person name={this.state.persons[1].name} age={this.state.persons[1].age} ></Person>
-          <Person name={this.state.persons[2].name} age={this.state.persons[2].age} >This is Chlidren</Person>
+          <br />
+          <button style={style} onClick={
+            this.toggleHandler.bind(this)
+          }>
+            Click for Toggle
+          </button>
+          {this.state.showPerson ?
+            // Please use only one child for conditional rendering
+            <div>
+              <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+              <Person name={this.state.persons[1].name} age={this.state.persons[1].age} ></Person>
+              <Person name={this.state.persons[2].name} age={this.state.persons[2].age} >This is Chlidren</Person>
+            </div>
+            : <br />}
           <i>{this.state.otherState}</i>
         </header>
       </div>
