4da4078bd0b6b6045a981e44a52a7a93ab9ccb2c
Chayut Ruksomya <chayut.r@bluepi.co.th>
2020-03-11 16:26:39 +0700

    [react] Use React.Fragment

--- react-js/first-react-app/src/components/Persons/Person/Person.js
+++ react-js/first-react-app/src/components/Persons/Person/Person.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import personStyle from './Person.module.css';
-import Aux from "../../../hoc/Auxiliary";
+// import Aux from "../../../hoc/Auxiliary";
 
 const person = (props) => {
     console.log('rendering...');
     return (
         // <div className={personStyle['person-item']}>
         // <Aux> component does the React.createElement() function call then send chidren pass down to itself.
-        <Aux>
+        // Same as <React.Fragment>
+        <React.Fragment>
             <p key="i1" onClick={props.click}>I'm Person | {props.name} | {props.age} | {props.children}</p>
             <input key="i2" onChange={props.change} value={props.name} type="text" />
-        </Aux>
+        </React.Fragment>
         // </div>
     )
 
