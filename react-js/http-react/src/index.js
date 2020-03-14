8a7d0e06c5b74779c7d47cad18310735bc646fd4
Chayut Ruksomya <chayut.191@gmail.com>
2020-03-14 12:47:55 +0700

    [react] Adding Interceptor to axios

--- react-js/http-react/src/index.js
+++ react-js/http-react/src/index.js
@@ -3,6 +3,30 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from "axios";
+
+// eslint-disable-next-line
+const myReqInterceptor = axios.interceptors.request.use(request => {
+    // NOTE : You can add more configuration before sending a request.
+    return request;
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+});
+
+// eslint-disable-next-line
+const myResInterceptor = axios.interceptors.response.use(response => {
+    // NOTE : You can add more configuration before recieve a response.
+    return response;
+}, error => {
+    console.log(error);
+    // By Promise.reject --> so you can catch an error locally
+    return Promise.reject(error);
+});
+
+// NOTE : call eject  with that reference as an argument, to remove it
+// axios.interceptors.request.eject(myReqInterceptor);
+// axios.interceptors.response.eject(myResInterceptor);
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
