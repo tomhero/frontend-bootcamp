6dfc778f565af810e3e40ca95bb8e522acdffa00
Chayut Ruksomya <chayut.191@gmail.com>
2020-03-15 19:45:24 +0700

    [rts] Add Routing...

--- react-js/burger-builder-app/src/App.tsx
+++ react-js/burger-builder-app/src/App.tsx
@@ -3,31 +3,22 @@ import React, { Component } from 'react';
 import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Checkout from "./containers/Checkout/Checkout";
+import { Route, Switch } from "react-router-dom";
 
 class App extends Component {
-
-  state = {
-    show: true
-  }
-
-  // componentDidMount() {
-  //   setTimeout(() => {
-  //     this.setState({show: false});
-  //     // NOTE : To test componentWillUnmount
-  //   }, 1500);
-  // }
-
   render() {
     return (
       <div className="App">
         <Layout>
-          {this.state.show ? <BurgerBuilder /> : null}
-          <Checkout />
+          {/* NOTE : You can see BrowserRouter in index.jsx file */}
+          <Switch>
+            <Route path="/checkout" exact={true} component={Checkout}/>
+            <Route path="/" component={BurgerBuilder}/>
+          </Switch>
         </Layout>
       </div>
     );
   }
-
 }
 
 export default App;
