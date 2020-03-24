da1d51da6b83d4720d8f5deedd37f001c088bf38
Chayut Ruksomya <chayut.r@bluepi.co.th>
2020-03-24 12:50:58 +0700

    [rts] ref and useRef

--- react-js/react-hooks/src/components/Ingredients/Search.js
+++ react-js/react-hooks/src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
@@ -6,32 +6,39 @@ import './Search.css';
 const Search = React.memo(props => {
   const { onLoadIngredients } = props;
   const [filterKeyword, setFilterKeyword] = useState('');
+  const inputRef = useRef();
 
   useEffect(() => {
-    const query = filterKeyword.length === 0
-      ? ''
-      : `?orderBy="title"&equalTo="${filterKeyword}"`;
-    fetch('https://react-learning-5f3ed.firebaseio.com/hook-ingredients.json' + query)
-      .then(response => response.json())
-      .then(ingredients => {
-        const loadedIngredients = [];
-        Object.keys(ingredients).forEach(ingredientKey => {
-          loadedIngredients.push({
-            id: ingredientKey,
-            title: ingredients[ingredientKey].title,
-            amount: ingredients[ingredientKey].amount
-          });
-        })
-        onLoadIngredients(loadedIngredients);
-      })
-  }, [filterKeyword, onLoadIngredients])
+    setTimeout(() => {
+      // Filtering text throll
+      if (filterKeyword === inputRef.current.value) {
+        const query = filterKeyword.length === 0
+          ? ''
+          : `?orderBy="title"&equalTo="${filterKeyword}"`;
+        fetch('https://react-learning-5f3ed.firebaseio.com/hook-ingredients.json' + query)
+          .then(response => response.json())
+          .then(ingredients => {
+            const loadedIngredients = [];
+            Object.keys(ingredients).forEach(ingredientKey => {
+              loadedIngredients.push({
+                id: ingredientKey,
+                title: ingredients[ingredientKey].title,
+                amount: ingredients[ingredientKey].amount
+              });
+            })
+            onLoadIngredients(loadedIngredients);
+          })
+      }
+    }, 500);
+  }, [filterKeyword, onLoadIngredients, inputRef])
 
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" 
+          <input type="text"
+            ref={inputRef}
             value={filterKeyword}
             onChange={event => setFilterKeyword(event.target.value)} />
         </div>
